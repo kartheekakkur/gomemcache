8d21c3aa15a905fc1cee9d128076960436949107
Vasudev <vasu@akkur.org>
2026-08-18 21:11:50 +0000

    adding LRU

--- cache.go
+++ cache.go
@@ -1,50 +1,77 @@
 package gomemcache
 
 import (
+	"container/list"
 	"sync"
 	"time"
 )
 
 type CacheItem struct {
-	Value string
+	Value      string
 	ExpiryTime time.Time
 }
 
 type Cache struct {
-	mu    sync.RWMutex
-	items map[string]CacheItem
+	mu       sync.RWMutex
+	items    map[string]*list.Element
+	eviction *list.List
+	capacity int
 }
 
-func NewCache() *Cache {
+type Entry struct {
+	key   string
+	value CacheItem
+}
+
+func NewCache(capacity int) *Cache {
 	return &Cache{
-		items: map[string]CacheItem{},
+		items:    make(map[string]*list.Element),
+		eviction: list.New(),
+		capacity: capacity,
 	}
 }
 
 func (c *Cache) Set(key, value string, ttl time.Duration) {
 	c.mu.Lock()
 	defer c.mu.Unlock()
-	c.items[key] = CacheItem{
-		Value: value,
-	    ExpiryTime: time.Now().Add(ttl),
+
+	if elem, found := c.items[key]; found {
+		c.eviction.Remove(elem)
+		delete(c.items, key)
 	}
+	if c.eviction.Len() >= c.capacity {
+		c.evictLRU()
+	}
+	item := CacheItem{
+		Value:      value,
+		ExpiryTime: time.Now().Add(ttl),
+	}
+
+	elem := c.eviction.PushFront(&Entry{key, item})
+	c.items[key] = elem
 }
 
 func (c *Cache) Get(key string) (string, bool) {
-	c.mu.RLock()
-	defer c.mu.RUnlock()
-	item, found := c.items[key]
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	elem, found := c.items[key]
 
-	if !found || time.Now().After(item.ExpiryTime){
-		return "", false
+	if !found || time.Now().After(elem.Value.(*Entry).value.ExpiryTime) {
+		if found {
+			c.eviction.Remove(elem)
+			delete(c.items,key)
+		}
+		return "",false
 	}
-	return item.Value, true
+
+	c.eviction.MoveToFront(elem)
+	return elem.Value.(*Entry).value.Value, true
 }
 
-func (c *Cache)StartEvictionTicker(d time.Duration){
+func (c *Cache) StartEvictionTicker(d time.Duration) {
 	ticker := time.NewTicker(d)
 
-	go func(){
+	go func() {
 		for range ticker.C {
 
 			c.evictExpiredItems()
@@ -52,14 +79,24 @@ func (c *Cache)StartEvictionTicker(d time.Duration){
 	}()
 }
 
-func (c *Cache) evictExpiredItems(){
+func (c *Cache) evictExpiredItems() {
 	c.mu.Lock()
 	defer c.mu.Unlock()
 
 	now := time.Now()
-	for key,item := range c.items{
-		if now.After(item.ExpiryTime){
-			delete(c.items,key)
+	for key, elem := range c.items {
+		if now.After(elem.Value.(*Entry).value.ExpiryTime) {
+			c.eviction.Remove(elem)
+			delete(c.items, key)
 		}
+	}
+}
+
+func (c *Cache) evictLRU() {
+	elem := c.eviction.Back()
+	if elem != nil {
+		c.eviction.Remove((elem))
+		kv := elem.Value.(*Entry)
+		delete(c.items, kv.key)
 	}
 }
